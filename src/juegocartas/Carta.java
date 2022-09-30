efb578a5e802b8a798895f86247b726183f2188b
Yellsmy <yltojg@alumno.uspg.edu.gt>
2022-09-29 21:24:41 -0600

    Contiene una carta

--- src/juegocartas/Carta.java
+++ src/juegocartas/Carta.java
@@ -1,14 +1,26 @@
 package juegocartas;
+/***********************************************
+* Producto.java
+* Yellsmy Toj
+*
+* Esta clase genérica detalla un producto 
+* tanto el código, el nombre y su precio de  
+* diferentes tipos de datos.
+***********************************************/
 
 public class Carta 
 {
     private String palo;
     private String identificadorCarta;
+    
+    //**************************************************************
 
     public Carta(String palo, String identificadorCarta) {
         this.palo = palo;
         this.identificadorCarta = identificadorCarta;
     }
+    
+    //**************************************************************
 
     public String getPalo() {
         return palo;
@@ -25,6 +37,8 @@ public class Carta
     public void setIdentificadorCarta(String identificadorCarta) {
         this.identificadorCarta = identificadorCarta;
     }
+    
+    //**************************************************************
 
     @Override
     public String toString() {
